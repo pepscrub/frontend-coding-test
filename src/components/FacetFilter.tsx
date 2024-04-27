f7dad29724a90c13a46bbe5a2a8bf1db3e1b52e8
Brendan <35904816+pepscrub@users.noreply.github.com>
2024-04-27 20:50:57 +1000

    initial solution

--- src/components/FacetFilter.tsx
+++ src/components/FacetFilter.tsx
@@ -1,6 +1,9 @@
-import { Button } from './ui/Button'
-import { TermsAggregation } from '../models/TermsAggregation'
+import { Bucket } from '@/models/Bucket'
+import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import { TermsAggregation } from '../models/TermsAggregation'
+import { useFilter } from './FacetFilterGroup'
+import { Button } from './ui/Button'
 
 export interface FacetFilterGroupProps {
   aggregation: TermsAggregation
@@ -8,29 +11,37 @@ export interface FacetFilterGroupProps {
 }
 
 export function FacetFilter({ aggregation }: { aggregation: TermsAggregation }) {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
+  const { updateFiltered } = useFilter();
   return (
     <div className={'p-2'}>
-      {aggregation?.buckets?.map((bucket) => {
-        return <FacetFilterItem bucket={bucket} />
+      {aggregation?.buckets?.map((bucket,i) => {
+        return <FacetFilterItem key={`${bucket.key}-${i}`} bucket={bucket} />
       })}
       <div className="flex py-2">
-        <Button size="sm">{t('Refine')}</Button>
+        <Button size="sm" onClick={updateFiltered}>{t('Refine')}</Button>
       </div>
     </div>
   )
 }
 
-export function FacetFilterItem({ bucket }: { bucket: any }) {
+export function FacetFilterItem({ bucket }: { bucket: Bucket }) {
+  const [checked, setChecked] = useState(false);
+  const { updateSelected } = useFilter();
+
+  // Disabled exhaustive deps due to infinite re-mounting on functions
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => updateSelected(bucket, checked), [bucket, checked]);
+
   return (
     <label
       className="flex text-sm small cursor-pointer py-1 hover:bg-slate-100 dark:hover:bg-slate-900"
       key={bucket.key}
     >
       <span className="mr-1">
-        <input type="checkbox" />
+        <input type="checkbox" checked={checked} onChange={() => setChecked((check) => !check)} />
       </span>
-      <span className="flex-1">{bucket.key.replaceAll('_', ' ')}</span>
+      <span className="flex-1">{bucket.key.replace('_', ' ')}</span>
       <span className="justify-self-end">({bucket.doc_count.toLocaleString()})</span>
     </label>
   )
--- src/components/FacetFilterGroup.tsx
+++ src/components/FacetFilterGroup.tsx
@@ -1,8 +1,79 @@
-import { useState } from 'react'
-import { FacetFilterGroupProps, FacetFilter } from './FacetFilter'
+import { useScholar } from '@/api';
+import { Bucket } from '@/models/Bucket';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { FC, PropsWithChildren, createContext, useContext, useState } from 'react';
+import { FacetFilter, FacetFilterGroupProps } from './FacetFilter';
 
-export function FacetFilterGroup({ aggregation, label }: FacetFilterGroupProps) {
+interface Props extends PropsWithChildren {}
+
+interface Context {
+  filteredList: Bucket[];
+  selected: Bucket[];
+  updateFiltered: () => void;
+  updateSelected: (bucket: Bucket, checked: boolean) => void;
+}
+
+const context = createContext<Context | undefined>(undefined);
+
+const FilterProvider: FC<Props> = ({ children }) => {
+  const [selected, setSelected] = useState<Bucket[]>([]);
+  const [filteredList, setFilteredList] = useState<Bucket[]>([]);
+
+  const updateSelected = (bucket: Bucket, checked: boolean) => {
+    if(!checked) return setSelected((selectedList) => selectedList.filter(({ key }) => key !== bucket.key));
+    setSelected((selection) => [...selection, bucket])
+  }
+
+  const updateFiltered = () => {
+    setFilteredList(selected)
+  }
+
+  return (
+    <context.Provider
+      value={{
+        filteredList,
+        selected,
+        updateFiltered,
+        updateSelected,
+      }}
+    >
+      {children}
+    </context.Provider>
+  )
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useFilter = (): Context => {
+  const name = FilterProvider.name;
+  const data = useContext(context);
+  if (!data) throw new Error(`Missing ${name} in tree above ${name}`);
+  return data;
+}
+
+export function FacetFilterGroup(props: FacetFilterGroupProps) {
+  return (
+    <QueryClientProvider client={new QueryClient}>
+      <FilterProvider>
+        <FacetFilterGroupModel {...props} />
+      </FilterProvider>
+    </QueryClientProvider>
+  )
+}
+
+export function FacetFilterGroupModel({ aggregation, label }: FacetFilterGroupProps) {
   const [show, setShow] = useState(true)
+  const { filteredList } = useFilter();
+  const filterKey = `${label.replace(/\s.*/, '').toLowerCase()}.must`;
+  const filter = encodeURI(filteredList.map(({ key }) => `${filterKey}=${key}`).join('&'));
+
+  const { data } = useScholar({
+      q: `aggregation.enabled:${show}${filter.length > 0 ? `&${filter}` : ''}`,
+      authorship: filterKey.includes('author') && !!filter.length,
+      inventorship: filterKey.includes('inventor') && !!filter.length
+    },
+    true,
+    1
+  );
   // TODO: useScholar with aggregation enabled:show
   return (
     <div className="ml-2 border">
