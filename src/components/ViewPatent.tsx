c4b60acf88c8169023e8a21d898a59d887b76ed1
Brendan <35904816+pepscrub@users.noreply.github.com>
2024-04-27 14:09:10 +1000

    Added first patent info tab and legal status info

--- src/components/ViewPatent.tsx
+++ src/components/ViewPatent.tsx
@@ -1,13 +1,63 @@
-import { PatentPills } from '@/components/PatentPills'
-import { PatentDocument } from '@/models/PatentDocument'
-import { useTranslation } from 'react-i18next'
+import { PatentDocument } from '@/models/PatentDocument';
+import { PatentEmojis } from '@/models/PatentLegalStatus';
+import { toTitleCase } from '@/utils';
+import { FC } from 'react';
+import { ReactCountryFlag } from 'react-country-flag';
+
+
+interface Props {
+  patent: PatentDocument;
+}
+
+const PatentBar: FC<Props> = ({ patent }) => {
+  const { jurisdiction, doc_number, kind, publication_type, priority_claim, family } = patent;
+  // Removes duplicate jurisdictions from array
+  const jurisdictions = Array.from(new Set(priority_claim.map(({ jurisdiction }) => jurisdiction)));
+  // Ideally this would be a translation
+  const titleCasedPublication = toTitleCase(publication_type);
+  return (
+    <div className={'flex'}>
+      <div className={'mr-5 flex items-center'}>
+        <ReactCountryFlag countryCode={jurisdiction} svg />
+        <span className={'mx-0.5 ml-1'}>{doc_number}</span>
+        <span className={'mx-0.5'}>{kind}</span>
+      </div>
+      <div className={'mr-5'}>
+        <span className={'font-semibold'}>{titleCasedPublication}</span>
+      </div>
+      {/* Figure out what the family length means? */}
+      <div className={'mr-5'}>
+        Family: {family.simple.size}
+      </div>
+      <div className={'mr-5'}>
+        <span>Family Jurisdictions:</span>
+        <span className={'px-1'}>{jurisdictions.join(',')}</span>
+      </div>
+    </div>
+  )
+}
+
+const PatentInfo: FC<Props> = ({ patent }) => {
+  const { legal_status: legal } = patent;
+  const legalStatus = legal.patent_status;
+  return (
+    <div>
+      <PatentBar patent={patent} />
+      <div>
+        Legal Status: <abbr title={legal.ipr_type}>{PatentEmojis[legalStatus]} {toTitleCase(legalStatus)}</abbr>
+      </div>
+    </div>
+  )
+}
+
 
 export function ViewPatent({ patent }: { patent: PatentDocument }) {
-  const { t } = useTranslation()
+  // const { t } = useTranslation()
   return (
     <div className={'container mx-auto py-5'}>
       <h1 className={'text-xl mb-2'}>{patent.title?.en?.at(0)?.text || ''}</h1>
-      <div>{patent.doc_key}</div>
+      <PatentInfo patent={patent} />
+      {/* <div>{patent.doc_key}</div>
       <p className={'capitalize py-2'}>{patent.publication_type}</p>
       <div className={'py-2'}>
         <PatentPills doc={patent} />
@@ -29,7 +79,7 @@ export function ViewPatent({ patent }: { patent: PatentDocument }) {
         {patent.inventor.map((inventor) => (
           <small key={inventor.name}>{inventor.name} </small>
         ))}
-      </div>
+      </div> */}
       <pre className={'py-2'}>{JSON.stringify(patent, null, 2)}</pre>
     </div>
   )
--- src/models/PatentLegalStatus.ts
+++ src/models/PatentLegalStatus.ts
@@ -1,1 +1,10 @@
 export type PatentLegalStatus = 'ACTIVE' | 'INACTIVE' | 'EXPIRED' | 'DISCONTINUED' | 'PENDING' | 'PATENTED' | 'UNKNOWN'
+export enum PatentEmojis {
+  'ACTIVE' = '🔴',
+  'DISCONTINUED' = '⚫',
+  'EXPIRED' = '🔵',
+  'INACTIVE' = '⚪',
+  'PATENTED' = '🟢',
+  'PENDING' = '🟠',
+  'UNKNOWN' = '🟣'
+}
